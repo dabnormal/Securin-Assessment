b41e2d8f22c93b134630c6dc0b8b113c22e804d6
dabnormal <yashjangid2904@gmail.com>
2025-01-27 20:20:30 +0530

    Update App.jsx
    path changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/cves/list" element={<CVEList />} />
+        <Route path="/" element={<CVEList />} />
         <Route path="/cves/:cveId" element={<CVEDetail />} />
       </Routes>
     </Router>
